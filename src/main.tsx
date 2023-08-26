d1dae93ef26088fdaefd8f4b4dcf653d2c7b3440
SamantaMelissa <61596646+SamantaMelissa@users.noreply.github.com>
2023-08-26 16:52:21 -0300

    Subindo atv de reforço REACT

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Header from './components/header'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Header/>
+  </React.StrictMode>,
+)
